0cf49aa89caa19e86061707177e657c9ea2bbc59
rodriguesz10x <guilherme10rodriguesz@gmail.com>
2025-06-03 12:04:01 -0300

    aula 43

--- Modulo 2/Aula 43/index.mjs
+++ Modulo 2/Aula 43/index.mjs
@@ -2,6 +2,23 @@ import express from 'express'
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
+import { Sequelize,DataTypes } from 'sequelize'
+
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,{
+        host:process.env.DB_HOST,
+        dialect:process.env.DB_DIALECT
+    }
+)
+
+try{
+    await sequelize.authenticate()
+    console.log('Banco conectado com sucesso!')  
+}catch(e){
+    console.log(e)
+}
 
 dotenv.config()
 
@@ -26,6 +43,50 @@ conectarBanco()
 
 app.get('/',(req,res)=>{
     return res.send('API inicial')
+})
+
+app.get('/todosUsuarios',(req,res)=>{
+    const conexao = conectarBanco()
+    let sql = 'select nome,tipo from usuarios'
+    conexao.query(sql,(e,result)=>{
+        if(e){
+            console.log(`deu erro: ${e}`)
+        }else{
+            return res.json(result)
+        }
+        conexao.end()
+    })
+    conexao.end()
+})
+
+app.get('/todosAlunos/:id',(req,res)=>{
+    const conexao = conectarBanco()
+    let id = req.params.id
+    let sql = `select * from usuarios where tipo = 'aluno' and id ${id}`
+    conexao.query(sql,(e,result)=>{
+        if(e){
+            console.log(`deu erro: ${e}`)
+        }else{
+            return res.json(result)
+        }
+        conexao.end()
+    })
+    conexao.end()
+})
+
+app.post('/cadastrarUsuario',(req,res)=>{
+    const conexao = conectarBanco()
+    let novoUsuario = req.body
+    let sql = `insert into usuarios (username, senha, nome, ativo, tipo) values ('${novoUsuario.username}','${novoUsuario.senha}','${novoUsuario.nome}','${novoUsuario.ativo}','${novoUsuario.tipo}')`
+
+    conexao.query(sql,(e,result)=>{
+        if(e){
+            console.log(e)            
+        }else{
+            res.status(200)
+            return res.send('Usuário Cadastrado!!')
+        }
+    })
 })
 
 app.listen(porta,()=>{
